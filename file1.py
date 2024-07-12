470a73cfadec0b0b2fa88f9ecf9852f06adaf081
bincy <bincysrinivaschowdariponduri@gmail.com>
2024-07-12 13:54:35 +0000

    Revert "-a"
    This reverts commit ac14a1c3cafd90aba87f950b45a1cb7ab880f6bc.

--- file1.py
+++ file1.py
@@ -1,5 +1,4 @@
 print("This is the sample program")
 #reading the name
-name=input("Enter your name:")
-#display the name
+name=inputt("Enter your name:")
 print(f"Welcome {name} to python programinig")
